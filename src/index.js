99845819fc8074556ae3700677d73299fd809f2f
AzSiAz <slider.perso@live.fr>
2016-10-31 14:22:07 +0100

    change export mode

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-const getSearchData = require('./parser/searchPage');
-const getSerieData = require('./parser/seriesPage');
-const getIndexData = require('./parser/indexPage');
-const getGroupData = require('./parser/groupParser');
-const getRankingData = require('./parser/seriesRankingParser');
-
-
 module.exports = {
-    getSearchData: getSearchData, 
-    getSerieData: getSerieData, 
-    getIndexData: getIndexData, 
-    getGroupData: getGroupData, 
-    getRankingData: getRankingData
+    getSearchData: require('./parser/searchPage'), 
+    getSerieData: require('./parser/seriesPage'), 
+    getIndexData: require('./parser/indexPage'), 
+    getGroupData: require('./parser/groupParser'), 
+    getRankingData: require('./parser/seriesRankingParser')
 };
